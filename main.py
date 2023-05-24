988f7957302c9066b59b7982924bd2f872ba27c5
guisena08 <guisena@ufc.alu.br>
2023-05-24 12:43:22 -0300

    tarefa concluida

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 """This program counts the number of vowels in a string."""
 
-def count_vowels(string:str) -> int:
-    """
-    Takes in a string and returns the number of vowels in the string.
 
-    Args:
-        string (str): The string to count vowels in.
+def count_vowels(string: str) -> int:
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in string:
+        if char in vowels:
+            count += 1
+    return count
 
-    Returns:
-        int: The number of vowels in the string.
-    """
+
+
+ 
